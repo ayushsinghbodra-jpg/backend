40af0a0d96083be614b587a0f5fee43d1e8705f2
Ayush_Kumar_Singh <ayushsingh.bodra@gmail.com>
2025-12-30 12:29:12 +0530

    made some changes

--- Chapter15 - Introduction to SQL/utils/databaseUtil.js
+++ Chapter15 - Introduction to SQL/utils/databaseUtil.js
@@ -1,9 +1,14 @@
+//we are creating databse using mysql2 package
+
 const mysql = require('mysql2');
 
+//pool is used to manage multiple database connections efficiently it allows us to reuse connections rather than creating a new one for each request
+//the syntax mysql.createPool is used to create a connection pool to the database
+//createPool method takes an object as an argument which contains the database connection details like host,user,password and database name
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Completecoding@01',
+  password: 'root',
   database: 'airbnb'
 });
 
